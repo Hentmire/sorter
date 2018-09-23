24efa8b3b08d9b75ad8f153564e5f77eda751a75
Victoria Udodava <43163503+Hentmire@users.noreply.github.com>
2018-09-23 18:51:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
+var arr = new Array(); 
 class Sorter {
+  
   constructor() {
-    // your implementation
+   this.arr = arr;
+   this.arr.length = 0;
   }
 
   add(element) {
-    // your implementation
+    this.arr.push(element);// your implementation
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];// your implementation
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;// your implementation
   }
 
   toArray() {
-    // your implementation
+    return this.arr;// your implementation
   }
 
   sort(indices) {
-    // your implementation
+    if (indices.length == 1) {
+      return this.arr;}
+    function compareNumbers(a, b) {
+      return a - b;
+    }
+    indices.sort(compareNumbers);
+    var massive = [];
+    for (var i = 0; i < indices.length; i++) {
+      massive[i] = this.arr[indices[i]];
+    }
+    massive.sort(compareNumbers);
+    for (var j = 0; j < indices.length; j++) {
+      this.arr[indices[j]] = massive[j];
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+   //return setComparator(compareFunction);
   }
-}
 
 module.exports = Sorter;
