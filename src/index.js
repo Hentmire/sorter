cd54d5e54c93a54df38ca97b4a74980e163b7e14
Victoria Udodava <43163503+Hentmire@users.noreply.github.com>
2018-09-24 17:26:31 +0300

    Correct solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+
 var arr = new Array(); 
+var func = 0;
 class Sorter {
   
   constructor() {
@@ -23,24 +25,34 @@ class Sorter {
   }
 
   sort(indices) {
+    
     if (indices.length == 1) {
       return this.arr;}
+    var compare;
+    if (func != 0) {
+      compare = func;
+    } else {
+      compare = function compareNumbers(a, b) {
+      return a - b;
+      }
+    }
     function compareNumbers(a, b) {
       return a - b;
-    }
+      }
     indices.sort(compareNumbers);
     var massive = [];
     for (var i = 0; i < indices.length; i++) {
       massive[i] = this.arr[indices[i]];
     }
-    massive.sort(compareNumbers);
+    massive.sort(compare);
     for (var j = 0; j < indices.length; j++) {
       this.arr[indices[j]] = massive[j];
     }
   }
 
   setComparator(compareFunction) {
-   //return setComparator(compareFunction);
+   return func = compareFunction;
   }
+}
 
 module.exports = Sorter;
